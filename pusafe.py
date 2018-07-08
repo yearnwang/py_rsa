5d720f895806e5de54f5ada91b6dafa2b3ae3a0c
yearnwang@126.com <yearnwang@126.com>
2018-07-08 19:27:38 +0800

    add aes

--- pusafe.py
+++ pusafe.py
@@ -1,6 +1,37 @@
 import rsa
 import base64
 
+from Crypto.Cipher import AES
+from Crypto import Random
+import random
+
+
+
+from hashlib import md5
+
+BS = 16
+
+
+def unicode_to_utf8(s):
+    if isinstance(s, unicode):
+        s = s.encode("utf-8")
+    return s
+
+
+def pad(s):
+    length = len(s)
+    add = BS - length % BS
+    byte = chr(BS - length % BS)
+    return s + (add * byte)
+
+
+def unpad(s):
+    length = len(s)
+    byte = s[length - 1:]
+    add = ord(byte)
+    return s[:-add]
+
+
 class PURsa(object):
 
     def __init__(self,_public_key_str,_private_key_str):
@@ -20,6 +51,32 @@ class PURsa(object):
         for i in range(0, len(buffer), length):
             res.append(rsa.decrypt(buffer[i:i + length], self.private_key))
         return "".join(res)
+        pass;
+
+
+class PUAes(object):
+    def __init__(self,_key,_mode = AES.MODE_CBC):
+
+        hash_md5 = md5(_key.encode('utf8'));
+        psw_key = hash_md5.hexdigest();
+
+        self.key = psw_key;
+        self.mode = _mode;
+        self.iv = Random.new().read(AES.block_size);
+        pass;
+
+    def encrypt(self,plaintext):
+        raw = unicode_to_utf8(plaintext)
+        raw = pad(raw)
+        cipher = AES.new(self.key, AES.MODE_CBC, self.iv)
+        return cipher.encrypt(raw)
+
+        pass;
+
+    def decrypt(self,ciphertext):
+        cipher = AES.new(self.key, AES.MODE_CBC, self.iv)
+        return unpad(cipher.decrypt(ciphertext))
+
         pass;
 
 
